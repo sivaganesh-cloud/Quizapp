33cc32b146ece06d470b5e2ce0ce4b5cbf26b0d2
sivaganesh-cloud <sivamadduri31@gmail.com>
2026-04-20 10:47:08 +0530

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    question: "What is HTML?",
+    answers: [
+      { text: "Programming Language", correct: false },
+      { text: "Markup Language", correct: true },
+      { text: "Database", correct: false },
+      { text: "Operating System", correct: false }
+    ]
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      { text: "Cascading Style Sheets", correct: true },
+      { text: "Computer Style Sheets", correct: false },
+      { text: "Creative Style System", correct: false },
+      { text: "Colorful Style Sheets", correct: false }
+    ]
+  }
+];
